4dcae143f82570e2d2dce55abe280a295e070017
pampapathi73 <pampapathik32@gmail.com>
2021-06-19 13:42:23 +0530

    [pampapathi] Added: Validate first name

--- UserRegistrationProblem-Using-Regex/Program.cs
+++ UserRegistrationProblem-Using-Regex/Program.cs
@@ -7,6 +7,13 @@ namespace UserRegistrationProblem_Using_Regex
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Registration problem");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            UserRegistration validateUser = new UserRegistration();
+            validateUser.validateFirstName("Pampapathi");
+            // validateUser.validateFirstName("Pampapathi");
+            Console.ReadKey();
         }
     }
 }
+
---
+++ UserRegistrationProblem-Using-Regex/UserRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem_Using_Regex
+{
+    class UserRegistration
+    {
+        public void validateFirstName(string firstName)
+        {
+            string stringForFirstName = "^[A-Z][a-z]{3,5}?";
+            if (Regex.IsMatch(firstName, stringForFirstName))
+                Console.WriteLine(firstName + " is Valid");
+            else
+                Console.WriteLine(firstName + " is Invalid");
+        }
+    }
+}
+
